797079b677bfc2095c72de2316d9e90399a97f73
bse173029 <iftisehar15@gmail.com>
2021-11-09 16:07:57 +0500

    my first servlet (practicing git)

---
+++ src/servlet/Servlet2.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet2")
+public class Servlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+            PrintWriter out = response.getWriter();
+            out.println("Servlet called");
+            out.println("Data Inserted Successfully");
+            String username = request.getParameter("username");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
